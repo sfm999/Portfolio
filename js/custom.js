519efbcd0147398dc3146c1f627ce027f12a9941
Stuart Miller <53849213+sfm999@users.noreply.github.com>
2023-01-18 20:29:21 +1100

    Added my own images and links to my github. Still need banner image of myself and not that random guy.

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,28 @@
-
 //
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				'verticalCentered': false,
-				'scrollingSpeed': 600,
-				'autoScrolling': false,
-				'css3': true,
-				'navigation': true,
-				'navigationPosition': 'right',
-			});
-		});
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    verticalCentered: false,
+    scrollingSpeed: 600,
+    autoScrolling: false,
+    css3: true,
+    navigation: true,
+    navigationPosition: "right",
+  });
+});
 
 // wow
-$(function()
-{
-    new WOW().init();
-    $(".rotate").textrotator();
-})
+$(function () {
+  new WOW().init();
+  $(".rotate").textrotator();
+});
+
+// Open multiple pages - RSA and SHA1
+$("a.RSA_SHA1").click(function (e) {
+  e.preventDefault();
+  window.open(
+    "https://github.com/sfm999/Cryptology/blob/Crypto-Main/RSA_Signature_Scheme/RSA_Signature_Schema.java"
+  );
+  window.open(
+    "https://github.com/sfm999/Cryptology/blob/Crypto-Main/RSA_Signature_Scheme/Modified_SHA1.java"
+  );
+});
---
+++ js/jquery.open-multiple-pages.js
@@ -0,0 +1,9 @@
+$("a.open_RSA").click(function (e) {
+  e.preventDefault();
+  window.open(
+    "https://github.com/sfm999/Cryptology/blob/Crypto-Main/RSA_Signature_Scheme/Modified_SHA1.java"
+  );
+  window.open(
+    "https://github.com/sfm999/Cryptology/blob/Crypto-Main/RSA_Signature_Scheme/RSA_Signature_Schema.java"
+  );
+});
